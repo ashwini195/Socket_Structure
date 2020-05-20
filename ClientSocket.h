8bbf2f0717f43778020c799064a38f56588a68b6
ashwini195 <63139299+ashwini195@users.noreply.github.com>
2020-05-20 16:39:14 +0530

    Add files via upload

---
+++ ClientSocket.h
@@ -0,0 +1,24 @@
+// Definition of the ClientSocket class
+
+#ifndef ClientSocket_class
+#define ClientSocket_class
+
+#include "Socket.h"
+//#define NDebug 
+
+
+
+class ClientSocket : private Socket
+{
+ public:
+
+  ClientSocket ( std::string host, int port );
+  virtual ~ClientSocket(){};
+
+  ClientSocket& operator << ( char* ) ;
+  ClientSocket& operator >> ( char* ) ;
+
+};
+
+
+#endif
